bdf78e3be5a204a138f32777da02e183aa197134
MarshP <marshp.vcs@gmail.com>
2018-10-21 19:27:32 +0100

    Update hello-world.py
    fun

---
+++ hello-world.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 21 18:33:56 2018
+
+@author: Marsh
+"""
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("Hello World")
+
+# Main program starts here
+hello()
